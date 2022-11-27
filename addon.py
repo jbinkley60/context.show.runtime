4c11b9a13e8ddfd975db0c5891f8855f58b31dd4
jbinkley60 <63779136+jbinkley60@users.noreply.github.com>
2022-11-26 19:25:05 -0500

    1.0.99g Test release

--- addon.py
+++ addon.py
@@ -3,6 +3,8 @@ import xbmc
 import xbmcaddon
 import xbmcgui
 import json
+from datetime import datetime, timedelta, time
+
 
 def convert_time(runtime):
     addon = xbmcaddon.Addon()
@@ -15,10 +17,13 @@ def display_details(addon, show_title, message):
     addon_name = addon.getAddonInfo('name')
     xbmcgui.Dialog().ok('{0} - {1}'.format(addon_name, show_title), message)
     
+def get_end_time(current_time_finish):
+    current_time_finish = datetime.now() + timedelta(seconds=finish_runtime)
+    return (current_time_finish)    
 
 if __name__ == '__main__':
     addon = xbmcaddon.Addon()
-    remaining_runtime, total_runtime = 0, 0
+    remaining_runtime, finish_runtime, total_runtime = 0,0, 0
     query = {"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "params": { "filter": { "field": "title", "operator": "is", "value": "" }, "limits": { "start": 0, "end": 1}, "properties": [ "title", "originaltitle", "playcount", "episode", "episodeguide", "watchedepisodes", "season"], "sort": { "order": "ascending", "method": "label"} }, "id": "libTvShows"}
     query = json.loads(json.dumps(query))
     query['params']['filter']['value'] = sys.listitem.getLabel()
@@ -39,12 +44,14 @@ if __name__ == '__main__':
         for episode in response['result']['episodes']:
             if episode['playcount'] == 0:
                 remaining_runtime += episode['runtime'] / 60
+                finish_runtime += episode['runtime']
             total_runtime += episode['runtime'] / 60
         
         xbmc.log('Context Showruntime remaining runtime: ' + str(show_title) + ' ' + str(remaining_runtime), xbmc.LOGDEBUG)
         xbmc.log('Context Showruntime total runtime: ' + str(show_title) + ' ' + str(total_runtime), xbmc.LOGDEBUG)
 
         remaining_runtime = convert_time(remaining_runtime)
+        finish_runtime = get_end_time(finish_runtime)
         total_runtime = convert_time(total_runtime)
 
         xbmc.log('Context Showruntime remaining runtime converted: ' + str(show_title) + ' ' + str(remaining_runtime), xbmc.LOGDEBUG)
